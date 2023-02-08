bffad9baf1f19e7a10491690c3e2bee3628f7764
Theo Truong <theotr@amazon.com>
2023-02-08 13:26:25 -0700

    POC

--- playground.rb
+++ playground.rb
@@ -3,7 +3,7 @@ require './lib/opensearch/api_generator/operation'
 require 'awesome_print'
 AwesomePrint.irb!
 
-generator = Class.new { include Opensearch::ApiGenerator }.new './api_specs/opensearch.openapi.json', './tmp'
+generator = Class.new { include Opensearch::ApiGenerator }.new './api_specs/opensearch.openapi.json', './poc'
 parser = generator.parser
 path_item = parser.paths['/_cat/indices/{index}']
 operation = path_item.get
@@ -12,7 +12,7 @@ op1 = Opensearch::ApiGenerator::Operation.new(operation, '/_cat/indices/{index}'
 op2 = Opensearch::ApiGenerator::Operation.new(operation, '/_cat/indices/{index}', 'post')
 
 require './lib/opensearch/api_generator/action_generator'
-action = Opensearch::ApiGenerator::ActionGenerator.new([op1, op2])
+action = Opensearch::ApiGenerator::ActionGenerator.new([op1])
 output = action.render
 puts output
-Pathname('./tmp/action.rb').write(output)
+Pathname('./poc/action.rb').write(output)
