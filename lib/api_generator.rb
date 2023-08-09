814054aa9b46e000cc264b592d6234b469225d86
Theo Truong <theotr@amazon.com>
2023-08-09 12:53:33 -0600

    Smarter way to get all existing namespaces

--- lib/api_generator.rb
+++ lib/api_generator.rb
@@ -16,38 +16,25 @@ require_relative 'index_generator'
 # Generate API endpoints for OpenSearch Ruby client
 class ApiGenerator
   HTTP_VERBS = %w[get post put patch delete patch head].freeze
-  EXISTING_NAMESPACES = Set.new(%w[
-                                  cluster
-                                  nodes
-                                  indices
-                                  ingest
-                                  snapshot
-                                  tasks
-                                  cat
-                                  remote
-                                  dangling_indices
-                                  features
-                                  shutdown
-                                ]).freeze
 
   # @param [String] openapi_spec location of the OpenSearch API spec file [required]
   def initialize(openapi_spec)
     @spec = Openapi3Parser.load_file(openapi_spec)
   end
 
-  # @param [String] output location of the API Gem folder (default to the parent folder of the generator)
+  # @param [String] gem_folder location of the API Gem folder (default to the parent folder of the generator)
   # @param [String] version target OpenSearch version to generate like "2.5" or "3.0"
   # @param [String] namespace namespace to generate (Default to all namespaces. Use '' for root)
   # @param [Array<String>] actions list of actions in the specified namespace to generate (Default to all actions)
-  def generate(output = '../', version: nil, namespace: nil, actions: nil)
-    gem_folder = Pathname output
-    namespaces = EXISTING_NAMESPACES.dup
+  def generate(gem_folder = '../', version: nil, namespace: nil, actions: nil)
+    gem_folder = Pathname gem_folder
+    namespaces = existing_namespaces(gem_folder)
     target_actions(version, namespace, actions).each do |action|
-      ActionGenerator.new(gem_folder + 'lib/opensearch/api/actions', action).generate
-      SpecGenerator.new(gem_folder + 'spec/opensearch/api/actions', action).generate
-      NamespaceGenerator.new(gem_folder + 'lib/opensearch/api/namespace', action.namespace).generate(namespaces)
+      ActionGenerator.new(gem_folder.join('lib/opensearch/api/actions'), action).generate
+      SpecGenerator.new(gem_folder.join('spec/opensearch/api/actions'), action).generate
+      NamespaceGenerator.new(gem_folder.join('lib/opensearch/api/namespace'), action.namespace).generate(namespaces)
     end
-    IndexGenerator.new(gem_folder + 'lib/opensearch', namespaces).generate
+    IndexGenerator.new(gem_folder.join('lib/opensearch'), namespaces).generate
   end
 
   private
@@ -63,6 +50,10 @@ class ApiGenerator
       end
     end.compact
 
-    operations.group_by(&:group).map { |group, ops| Action.new ops }
+    operations.group_by(&:group).values.map { |ops| Action.new ops }
+  end
+
+  def existing_namespaces(gem_folder)
+    gem_folder.join('lib/opensearch/api/actions').children.select(&:directory?).map(&:basename).map(&:to_s).to_set
   end
 end
