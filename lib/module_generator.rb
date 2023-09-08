31f34a443d61635d687b0bf4dea4d5792ae6e4a0
Theo Truong <theotr@amazon.com>
2023-09-08 14:19:34 -0600

    JS - module generator (init)

---
+++ lib/module_generator.rb
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+
+# frozen_string_literal: true
+
+require_relative 'base_generator'
+require_relative 'method_generator'
+
+# Generate a Namespace file via Mustache
+class ModuleGenerator < BaseGenerator
+  self.template_file = './templates/module.mustache'
+  attr_reader :namespace, :camelized_namespace
+
+  # @param [Pathname] output_folder Output folder
+  # @param [String] namespace Namespace name
+  # @param [Array<Action>] actions Actions in the namespace
+  def initialize(output_folder, namespace, actions)
+    super(output_folder)
+    @namespace = namespace
+    @camelized_namespace = @namespace.camelcase
+    @actions = actions
+  end
+
+  def query_name_mapping
+    @actions.flat_map(&:query_params).uniq(&:name).map { |p| { camel_name: p.camel_name, snake_name: p.snake_name } }
+  end
+
+  def method_definitions
+    @actions.sort_by(&:name).map do |action|
+      MethodGenerator.new(@output_folder, action).render
+    end.join("\n\n")
+  end
+
+  private
+
+  def output_file
+    create_folder(@output_folder).join("#{@namespace}.js")
+  end
+end
